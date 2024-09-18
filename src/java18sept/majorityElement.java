ca3bac776ea0ba7b75e4dd1e2d8043fd8de608c9
aditya25122005 <144265921+aditya25122005@users.noreply.github.com>
2024-09-18 16:50:25 +0530

    new

---
+++ src/java18sept/majorityElement.java
@@ -0,0 +1,26 @@
+package java18sept;
+
+public class majorityElement {
+    public static void main(String[] args) {
+        int[] nums={2,2,1,1,1,2,2};
+        System.out.println(moorevoting(nums));
+    }
+    public static  int  moorevoting(int [] nums){
+        int e= nums[0];
+        int v=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==e){
+                v++;
+            }
+            else{
+                v--;
+                if(v==0){
+                    e=nums[i];
+                    v=1;
+                }
+            }
+        }
+        return e;
+
+    }
+}
