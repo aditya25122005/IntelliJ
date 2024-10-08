5466696a960837bdbfc873636b2258d02017f0a0
aditya25122005 <aditya25pda@gmail.com>
2024-10-08 23:31:43 +0530

    new

---
+++ src/assignment3_CB/inverse_of_array.java
@@ -0,0 +1,23 @@
+package assignment3_CB;
+import java.util.*;
+public class inverse_of_array {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int [] arr=new int[N];
+        int [] inverse=new int[N];
+        for(int i=0;i<N;i++){
+            arr[i]=sc.nextInt();
+
+        }
+        for(int i=0;i<N;i++){
+            inverse[arr[i]]=i;
+        }
+        for(int i=0;i<N;i++){
+            System.out.print(inverse[i]+" ");
+        }
+
+
+    }
+}
