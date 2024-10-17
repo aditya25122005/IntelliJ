34aa65fef37d099c405492d7d7c53818e1ca6757
aditya25122005 <aditya25pda@gmail.com>
2024-10-17 21:39:15 +0530

    new

--- src/java_16_oct/stair_case_search.java
+++ src/java_16_oct/stair_case_search.java
@@ -2,7 +2,11 @@ package java_16_oct;
 
 public class stair_case_search {
     public static void main(String[] args) {
-      int [][] arr=  {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+      int [][] arr=  {{1,4,7,11,15},
+                      {2,5,8,12,19},
+                      {3,6,9,16,22},
+                      {10,13,14,17,24},
+                      {18,21,23,26,30}};
       int item=13;
         System.out.println(search(arr,item));
     }
