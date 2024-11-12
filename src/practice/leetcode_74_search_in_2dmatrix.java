d67735ae8b9beae47e305384dc9e1754c8df7c82
aditya25122005 <aditya25pda@gmail.com>
2024-11-12 23:53:07 +0530

    new

---
+++ src/practice/leetcode_74_search_in_2dmatrix.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class leetcode_74_search_in_2dmatrix {
+    public static void main(String[] args) {
+        int[] []matrix={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int target=16;
+        System.out.println(searchMatrix(matrix,target));
+
+    }
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int row=0;
+        int col=matrix[0].length-1;
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col]==target){
+                return true;
+            }
+            else if(matrix[row][col]>target){
+                col--;
+            }
+            else{
+                row++;
+            }
+
+        }
+        return false;
+    }
+}
