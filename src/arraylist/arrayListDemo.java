97744c4577334bda6d886f261f6ff9c4acc90fcc
aditya25122005 <aditya25pda@gmail.com>
2024-11-21 07:50:18 +0530

    new

---
+++ src/arraylist/arrayListDemo.java
@@ -0,0 +1,36 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class arrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer>l1=new ArrayList<>(99);
+        l1.add(10);    //O(1)
+        l1.add(20);
+        l1.add(30);
+        System.out.println(l1);
+        l1.add(1,9);  //O(N)
+        System.out.println(l1);
+        l1.addLast(50);
+        System.out.println(l1);
+
+        System.out.println(l1.get(2));
+        l1.remove(2);
+        System.out.println(l1);
+        System.out.println(l1.size());
+        l1.set(1,101);
+        System.out.println(l1);
+//        for(int i=0;i<l1.size();i++){
+//            System.out.print(l1.get(i)+" ");
+//        }
+        for(int ele:l1){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+        Collections.sort(l1);
+        System.out.println(l1);
+
+
+    }
+}
---
+++ src/string/string_to_number.java
@@ -0,0 +1,9 @@
+package string;
+
+public class string_to_number {
+    String str="546789";
+    int num=Integer.parseInt(str);
+    String str1="563324567";
+    long num1=Long.parseLong(str1);
+
+}
