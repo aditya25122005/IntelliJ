296b4c19092f3f06248f5442c7d4af6ca4c3fa37
aditya25122005 <aditya25pda@gmail.com>
2025-01-23 22:13:48 +0530

    new

---
+++ src/winter_arc/leetcode_73.java
@@ -0,0 +1,29 @@
+package winter_arc;
+
+public class leetcode_73 {
+    public static void main(String[] args) {
+        int [][] matrix={{1,1,1},{1,0,1},{1,1,1}};
+        setZeroes(matrix);
+    }
+    public static void setZeroes(int[][] matrix) {
+        boolean[] row=new boolean[matrix.length];
+        boolean[] col=new boolean[matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    row[i]=true;
+                    col[j]=true;
+
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(row[i]==true || col[j]==true){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+    }
+}
